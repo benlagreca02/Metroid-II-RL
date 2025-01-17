28f449afc5ee3dfbf317a7590ac73a47fe62ef7d
Benjamin LaGreca <benlagreca02@gmail.com>
2025-01-17 09:11:22 -0500

    Taking random actions!

--- README.md
+++ README.md
@@ -6,7 +6,14 @@ This is my attempt at having reinforcemnt learning algo play Metroid II
 lots to do...
 
 - [ ] Make and verify a custom environment with PyBoy
-    - [ ] Stub out the bare bones
-    - [ ] Integrate the RAM mappings 
-- [ ] Train a model
+    - [x] Verify ROM integrity
+    - [x] Take random actions
+    - [ ] Implement a `game_wrapper` for Metroid II (makes AI stuff easier)
+        - [ ] Integrate and verify the RAM mappings 
+    - [ ] Figure out all possible button input combos and implement them (We
+      want to be able to jump and shoot, or aim up, run, and shoot at the same
+      time possibly)
+
+- [ ] Model
+    - [ ] Research, research, research...
 - [ ] Containerize the program to make running on other machines easy
--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-
-
+from pyboy import PyBoy
 from metroid_env import MetroidEnv
 
+ROM_PATH = "MetroidII.gb"
+
 def main():
-    env = MetroidEnv()
+    pyboy = PyBoy(ROM_PATH)
+    env = MetroidEnv(pyboy)
+
+    # Just take random action for now
+    for _ in range(1000000):
+        # this is where you would insert your policy
+        action = env.action_space.sample()
+
+        # step (transition) through the environment with the action
+        # receiving the next observation, reward and if the episode has terminated or truncated
+        observation, reward, terminated, truncated, info = env.step(action)
+
+
+        # If the episode has ended then we can reset to start a new episode
+        if terminated or truncated:
+            observation, info = env.reset()
+
+    env.close()
 
 
 
