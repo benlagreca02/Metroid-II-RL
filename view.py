d53979730719d58ba7df068f1cff57e23d0322b9
Benjamin LaGreca <benlagreca02@gmail.com>
2025-02-11 14:31:23 -0500

    Improved args to allow human play mode if no args provided, next I want to add debug print command line args

--- view.py
+++ view.py
@@ -1,31 +1,43 @@
 # TODO write code to load and watch a model perform here
 import argparse
 
-import gymnasium as gym
-from stable_baselines3 import PPO
-from metroid_env import MetroidEnv, DEFAULT_NUM_TO_TICK
-
+def get_action_from_model(model, obs):
+    action, _states = model.predict(obs)
+    return action, _states
+        
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Path to model to load")
-    parser.add_argument('model_path', type=str, help='Path to the model to be loaded')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model_path', nargs='?', type=str, help='Path to the model to be loaded')
     
     args = parser.parse_args()
     
-    model_path = args.model_path
+    if not args.model_path:
+        print("No model given, using human control!")
 
-    # TODO make this script take command line arg for loading model
-    env = MetroidEnv(render_mode='human',
-            emulation_speed_factor=1,
-            num_to_tick=DEFAULT_NUM_TO_TICK)
+    # TODO move these back up to the top when done prototyping with path loading
+    import gymnasium as gym
+    from stable_baselines3 import PPO
+    from metroid_env import MetroidEnv, DEFAULT_NUM_TO_TICK
 
-    model= PPO("MlpPolicy", env, verbose=1)
-    
-    
-    model = PPO.load(model_path)
+    env = MetroidEnv(
+            render_mode='human',
+            emulation_speed_factor=1, # Because we want to watch it, run in "real time"
+            num_to_tick= 1 if not args.model_path else DEFAULT_NUM_TO_TICK)
+
+    if args.model_path:
+        print("Loading model from %s" % args.model_path)
+        model= PPO("CnnPolicy", env, verbose=1) 
+        model = PPO.load(args.model_path)
+        action_getter = lambda obs: get_action_from_model(model, obs)
+    else:
+        print("Using human player mode")
+        action_getter = lambda obs: (0,None)
+
 
     obs, info = env.reset()
     while True:
-        action, _states = model.predict(obs)
+        # action, _states = model.predict(obs)
+        action, _states = action_getter(obs)
         obs, rewards, dones, truncated, info = env.step(action)
 
