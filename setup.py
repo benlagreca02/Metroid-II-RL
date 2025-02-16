a7c057249f379baf8c030c7139e69d604fd8ab1d
Benjamin LaGreca <benlagreca02@gmail.com>
2025-02-16 13:23:56 -0500

    added setup.py to allow for Defaulting to user installation because normal site-packages is not writeable Obtaining file:///home/ben/school/graduateProject/code/Metroid-II-RL   Preparing metadata (setup.py): started   Preparing metadata (setup.py): finished with status 'done' Requirement already satisfied: gymnasium>=0.29.1 in /usr/local/lib/python3.10/dist-packages (from metroid-ii-rl==0.0.0) (0.29.1) Requirement already satisfied: numpy in /home/ben/.local/lib/python3.10/site-packages (from metroid-ii-rl==0.0.0) (1.26.4) Requirement already satisfied: cloudpickle>=1.2.0 in /home/ben/.local/lib/python3.10/site-packages (from gymnasium>=0.29.1->metroid-ii-rl==0.0.0) (3.1.1) Requirement already satisfied: farama-notifications>=0.0.1 in /home/ben/.local/lib/python3.10/site-packages (from gymnasium>=0.29.1->metroid-ii-rl==0.0.0) (0.0.4) Requirement already satisfied: typing-extensions>=4.3.0 in /home/ben/.local/lib/python3.10/site-packages (from gymnasium>=0.29.1->metroid-ii-rl==0.0.0) (4.12.2) Installing collected packages: metroid-ii-rl   Attempting uninstall: metroid-ii-rl     Found existing installation: metroid-ii-rl 0.0.0     Uninstalling metroid-ii-rl-0.0.0:       Successfully uninstalled metroid-ii-rl-0.0.0   Running setup.py develop for metroid-ii-rl Successfully installed metroid-ii-rl to expose so pufferlib can use

---
+++ setup.py
@@ -0,0 +1,39 @@
+# This script allows my code to be installed using `pip3 install -e .`
+
+from setuptools import find_packages, setup
+from itertools import chain
+
+setup(
+    name="metroid_ii_rl",
+    description="Metroid II Pufferlib Gymnasium env. for RL",
+    long_description_content_type="text/markdown",
+    # version=open('Metroid-II-RL/version.py').read().split()[-1].strip("'"),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # TODO make pyboy a dependency
+        # Manually installed from my fork for now
+        # 'pyboy<2.0.0',
+        'gymnasium>=0.29.1',
+        'numpy',
+    ],
+
+    # entry_points = {
+    #     'console_scripts': [
+    #         'pokegym.play = pokegym.environment:play'
+    #     ]
+    # },
+    python_requires=">=3.10",
+    license="MIT",
+    # @pdubs: Put your info here
+    author="Ben LaGreca",
+    author_email="benlagreca02@gmail.com",
+    url="https://github.com/PufferAI/pokegym",
+    keywords=["Metroid II", "AI", "RL"],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
